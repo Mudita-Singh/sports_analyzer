d87b1c4f7775211427e6a972cf9c4412ea64c5ee
Mudita <smudita900@gmail.com>
2026-03-30 18:13:05 +0530

    fix: windows startup and type safety refinements

--- server/storage.ts
+++ server/storage.ts
@@ -32,7 +32,7 @@ export class MemStorage implements IStorage {
 
   async createUser(insertUser: InsertUser): Promise<User> {
     const id = randomUUID();
-    const user: User = { ...insertUser, id };
+    const user: User = { ...insertUser, id, isGuest: insertUser.isGuest ?? "false" };
     this.users.set(id, user);
     return user;
   }
